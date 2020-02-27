4064eef9175af922be303565feb7b695ced490ed
phintaC <46598112+phintaC@users.noreply.github.com>
2020-02-27 17:40:29 +0100

    Add files via upload
    Creates CSV file for use with KMA: converts paths of raw data to that of trimmed data from an input CSV file.

---
+++ update_path.py
@@ -0,0 +1,36 @@
+#!usr/bin/env python3
+import os, sys, re
+# Define extension tuple
+fqext = (".fq.gz", ".fastq.gz")
+
+# Prompt for stuff
+file_in = input("Please provide csv file of paths for raw files: ")
+delim = input("Please specify delimiter (e.g. \",\", \"tab\"...): ")
+if delim == "tab":
+	delim = "\t"
+
+path_out = input("Please provide replacement path: ")
+file_out = input("Name your output csv file: ") + ".csv"
+
+with open(file_out, "w") as outfile:
+	outfile.write("")
+
+# Replace paths
+with open(file_in, "r") as infile: 
+	for line in infile:
+		# Splits the columns on delimiter
+		r1_col = line.split(delim)[0]
+		r2_col = line.split(delim)[1]
+		
+		# Extracts the file names and replaces extension
+		r1_trim = r1_col.split("/")[-1].replace(".fastq.gz", ".trim.fq.gz")
+		r2_trim = r2_col.split("/")[-1].replace(".fastq.gz", ".trim.fq.gz")
+
+		# Creates singletons from r1
+		singletons = r1_col.split("/")[-1].replace(".fastq.gz", ".singletons.fq.gz")
+
+		# Writes to file
+		with open(file_out, "a") as outfile: 
+				outfile.write(path_out + r1_trim + "\t" +\
+					path_out + r2_trim + "\t" +\
+					path_out + singletons + "\n")
