ae5b6687c8303c7f1010fc42c103db81e03e9955
phintaC <46598112+phintaC@users.noreply.github.com>
2020-03-01 16:30:09 +0100

    Add files via upload

--- update_path_2018.py
+++ update_path_2018.py
@@ -4,14 +4,11 @@ import os, sys, re
 fqext = (".fq.gz", ".fastq.gz")
 
 # Prompt for stuff
-path = "/home/projects/cge/backup/fastq/cge_private/FOOD_seqdata/HiSeq/Metagenomics/Trimmed_data/"
+path_out = "/home/projects/cge/backup/fastq/cge_private/FOOD_seqdata/HiSeq/Metagenomics/Trimmed_data/2019_Flowcell_1/"
 
-file_in = "/home/phine/Documents/DTU/thesis/data/unpublished/" + input("Please provide csv file of paths for raw files: ")
-delim = input("Please specify delimiter (e.g. \",\", \"tab\"...): ")
-if delim == "tab":
-	delim = "\t"
+file_in = "/home/phine/Documents/DTU/thesis/data/unpublished/paths_jun2018.csv"
+delim = ","
 
-path_out = "/home/projects/cge/backup/fastq/cge_private/FOOD_seqdata/HiSeq/Metagenomics/Trimmed_data/" + input("Please provide replacement path (from \"/Trimmed_data/\": ")
 file_out = input("Name your output csv file: ") + ".csv"
 
 with open(file_out, "w") as outfile:
@@ -24,6 +21,9 @@ with open(file_in, "r") as infile:
 		r1_col = line.split(delim)[0].rstrip()
 		r2_col = line.split(delim)[1].rstrip()
 		
+		# Extract filename for directory access
+		trim_dir = path_out + r1_col.split("/")[-1].replace("_R1.fastq.gz", "_trim/")
+
 		# Extracts the file names and replaces extension
 		r1_trim = r1_col.split("/")[-1].replace(".fastq.gz", ".trim.fq.gz")
 		r2_trim = r2_col.split("/")[-1].replace(".fastq.gz", ".trim.fq.gz")
@@ -33,6 +33,6 @@ with open(file_in, "r") as infile:
 
 		# Writes to file
 		with open(file_out, "a") as outfile: 
-				outfile.write(path_out + r1_trim + "\t" +\
-					path_out + r2_trim + "\t" +\
-					path_out + singletons + "\n")
+				outfile.write(trim_dir + r1_trim + "\t" +\
+					trim_dir + r2_trim + "\t" +\
+					trim_dir + singletons + "\n")
